3221155ebdef3e6670938018365b558879c75bcd
오율산 (Yulsan Oh) <156407033+yulsanoh@users.noreply.github.com>
2024-07-17 16:28:23 +0900

    Update App.tsx

--- framer-motion/src/App.tsx
+++ framer-motion/src/App.tsx
@@ -12,8 +12,7 @@ const Wrapper = styled(motion.div)`
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   display: flex;
@@ -24,27 +23,44 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: skyblue;
-  width: 100px;
-  height: 100px;
-  border-radius: 50px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-
-  const toggleClick = () => {
-    setClicked((prev) => !prev);
-  };
-
+  const [id, setId] = useState<null | string>(null);
+  console.log(id);
   return (
     <>
-      <Wrapper onClick={toggleClick}>
+      <Wrapper>
+        <Grid>
+          {["1", "2", "3", "4"].map((n) => (
+            <Box onClick={() => setId(n)} key={n} layoutId={n} />
+          ))}
+        </Grid>
         <AnimatePresence>
-          <Box>{clicked ? <Circle layoutId="circle" /> : null}</Box>
-          <Box>{clicked ? null : <Circle layoutId="circle" />}</Box>
+          {id ? (
+            <Overlay onClick={() => setId(null)} initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }} animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }} exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}>
+              <Box layoutId={id} style={{ width: 400, height: 200 }} />
+            </Overlay>
+          ) : null}
         </AnimatePresence>
       </Wrapper>
     </>
